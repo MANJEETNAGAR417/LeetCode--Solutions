1892313874a51c5eb8730b285fe075edf1994c4f
Manjeet <manjeet02220@gmail.com>
2026-07-29 14:38:53 +0530

    Time: 3 ms (7.96%), Space: 29.7 MB (46.79%) - LeetHub

--- 0189-rotate-array/0189-rotate-array.cpp
+++ 0189-rotate-array/0189-rotate-array.cpp
@@ -4,15 +4,8 @@ public:
         int n=nums.size();
         int K=k%n;
         if(n==1 || K==0)return;
-        vector<int>temp;
-        for(int i=n-K;i<n;i++){
-            temp.push_back(nums[i]);
-        }
-        for(int i=0;i<n-K;i++){
-            temp.push_back(nums[i]);
-        }
-        for(int i=0;i<n;i++){
-            nums[i]=temp[i];
-        }
+        reverse(nums.begin(),nums.begin()+n-K);
+        reverse(nums.begin()+n-K,nums.end());
+        reverse(nums.begin(),nums.end());
     }
 };
